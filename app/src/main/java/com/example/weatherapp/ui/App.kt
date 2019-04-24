af00cbf1a7eebb798fd763fea38d07a4a749399d
wangzhichao <wangzhichao@adups.com>
2019-04-24 20:45:09 +0800

    custom delegate

--- app/src/main/java/com/example/weatherapp/ui/App.kt
+++ app/src/main/java/com/example/weatherapp/ui/App.kt
@@ -1,6 +1,7 @@
 package com.example.weatherapp.ui
 
 import android.app.Application
+import com.example.weatherapp.ui.util.DelegatesExt
 
 /**
  *
@@ -9,8 +10,11 @@ import android.app.Application
  */
 class App : Application() {
     companion object {
-        private var instance: Application? = null
-        fun instance() = instance!!
+//        private var instance: Application? = null
+//        fun instance() = instance!!
+        // 使用委托属性
+        var instance: App by DelegatesExt.notNullSingleValue()
+//        var instance: App by Delegates.notNull()
     }
 
     override fun onCreate() {
@@ -25,4 +29,5 @@ class App : Application() {
  * 伴生对象的成员方法还是自己的成员方法。
  * 2, 伴生对象是在类中创建的一个单例。
  * 3，!! 是 非空断言，如果 instance 不是 null，那么还返回自己，如果为空，就抛出 NPE 。
+ * 4，by 关键字，用来指定一个委托对象。
  */
---
+++ app/src/main/java/com/example/weatherapp/ui/util/DelegatesExt.kt
@@ -0,0 +1,47 @@
+package com.example.weatherapp.ui.util
+
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+/**
+ *
+ * @author wzc
+ * @date 2019/4/24
+ */
+object DelegatesExt {
+    fun <T> notNullSingleValue(): ReadWriteProperty<Any?, T> = NotNullSingleValueVar()
+}
+
+class NotNullSingleValueVar<T>() : ReadWriteProperty<Any?, T> {
+    private var value: T? = null
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T =
+        // getter 函数 如果已经被初始化，则会返回一个值，否则抛出异常
+        value ?: throw IllegalStateException("${property.name} not initialized")
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        // setter 函数 如果值仍然是 null，则进行赋值；否则抛出异常
+        this.value = if (this.value == null) value else
+            throw IllegalStateException("${property.name} already initialized")
+    }
+}
+
+/**
+ * 总结：
+ * 1，示例中的代码为什么是下边这样的？
+ *
+ * class NotNullSingleValueVar<T> {
+
+private var value: T? = null
+
+operator fun getValue(thisRef: Any?, property: KProperty<*>): T =
+value ?: throw IllegalStateException("${property.name} not initialized")
+
+operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+this.value = if (this.value == null) value
+else throw IllegalStateException("${property.name} already initialized")
+}
+}
+ * 2, 这里是一个自定义的委托：
+ * val 属性实现 ReadOnlyProperty 接口，var 属性实现 ReadWriteProperty 接口。
+ * */
